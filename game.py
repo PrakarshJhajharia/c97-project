3eced34ccc08cacf1857d5e7010a9a70f688c373
PrakarshJhajharia <63593716+PrakarshJhajharia@users.noreply.github.com>
2020-12-01 20:20:05 +0530

    Update game.py

--- game.py
+++ game.py
@@ -1,14 +1,24 @@
 import random
 a = int(input("Pls guess a number between 1 and 9 : "))
 b = random.randint(0, 9)
-for i in range(101,1,-1):
+c = 0
+for i in range(5,1,-1):
     if (a == b):
         print("Congrats! you won")
+        c = 1
+        i = 0
         break;
+        
+        
     elif a>b:
         print("Your guess was too high. Guess a number lower than ",a)
-        a = int(input("Your guess"))
+        a = int(input("Your guess : "))
     elif a<b:
         print("Your guess was too low. Guess a number higher than ",a)
-        a = int(input("Your guess"))
+        a = int(input("Your guess : "))
+if (a == b and c == 0):
+        print("Congrats! you won")
+        i = 1
+if i == 2:
+    print("you loose")
 
